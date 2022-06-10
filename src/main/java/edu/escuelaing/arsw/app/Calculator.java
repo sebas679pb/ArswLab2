a19be0096fee49214de466b3dadaf808e9ac6b4d
Sebastian Piñeros <sebas679pb>
2022-06-09 19:45:44 -0500

    mean method impl

--- src/main/java/edu/escuelaing/arsw/app/Calculator.java
+++ src/main/java/edu/escuelaing/arsw/app/Calculator.java
@@ -5,25 +5,31 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Calculator{
-    
+public class Calculator {
+
     private static List<Double> numbersList = new LinkedList<Double>();
 
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
         fileConverter(args[0]);
+        System.out.println(mean(numbersList));
     }
 
-    public static void fileConverter(String path) throws Exception{
+    public static void fileConverter(String path) throws Exception {
         File file = new File(path);
         Scanner scan = new Scanner(file);
-        while (scan.hasNextLine()){
+        while (scan.hasNextLine()) {
             String number = scan.nextLine().strip();
             numbersList.add(Double.parseDouble(number));
         }
     }
 
-    public static void mean(List<?> numList) {
-        
+    public static Double mean(List<Double> numList) {
+        double mean = 0;
+        for (int i = 0; i < numList.size(); i++) {
+            mean += numList.get(i);
+        }
+        mean = Math.round((mean / numList.size()) * 100.0) / 100.0;
+        return mean;
     }
 
 }

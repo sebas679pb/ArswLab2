47b1cc7b765db15790254dd71121aa9f98884b75
Sebastian Piñeros <sebas679pb>
2022-06-10 03:17:01 -0500

    Tests impl

--- src/main/java/edu/escuelaing/arsw/app/Calculator.java
+++ src/main/java/edu/escuelaing/arsw/app/Calculator.java
@@ -9,22 +9,40 @@ public class Calculator {
 
     private static List<Double> numbersList = new OpLinkedList<Double>();
 
+    /**
+     * Programa que calcula la media y la desviacion estandar dado un archivo de numeros.
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        fileConverter(args[0]);
+        numbersList = fileConverter(args[0]);
         System.out.println(mean(numbersList));
         System.out.println(standardDeviation(numbersList));
     }
 
-    public static void fileConverter(String path) throws Exception {
+    /**
+     * Metodo que lee un archivo y digita los numeros en una lista enlazada.
+     * @param path as String
+     * @return Lista enlazada de numeros.
+     * @throws Exception
+     */
+    public static List<Double> fileConverter(String path) throws Exception {
+        List<Double> numList = new OpLinkedList<Double>();
         File file = new File(path);
         Scanner scan = new Scanner(file);
         while (scan.hasNextLine()) {
             String number = scan.nextLine().strip();
-            numbersList.add(Double.parseDouble(number));
+            numList.add(Double.parseDouble(number));
         }
         scan.close();
+        return numList;
     }
 
+    /**
+     * Metodo que calcula la media dada una lista de numeros.
+     * @param numList as linkedList.
+     * @return media de la lista.
+     */
     public static Double mean(List<Double> numList) {
         double mean = 0;
         for (Double num : numList) {
@@ -34,7 +52,12 @@ public class Calculator {
         return mean;
     }
 
-    public static double standardDeviation(List<Double> numList) {
+    /**
+     * Metodo que calcula la desviacion estandar dada una lista de numeros.
+     * @param numList as linkedList.
+     * @return desviacion estandar de la lista.
+     */
+    public static Double standardDeviation(List<Double> numList) {
         Double mean = mean(numList);
         Double sd = (double) 0;
         for (Double num : numList) {
